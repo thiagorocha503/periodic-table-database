e65502230b9368ccefd6997fcda4b0bae45cb46a
Thiago Ferreira <thiagorocha503@gmail.com>
2023-06-24 13:30:33 -0300

    feat: Create element.sh

---
+++ element.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
+# if symbol or name
+if [[  $1 =~ [a-zA-Z]+ ]]
+then
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN  properties USING(atomic_number) WHERE name='$1' OR symbol='$1' ")
+fi
+# if atomic number
+if [[ $1 =~ [0-9]+ ]]
+then
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN  properties USING(atomic_number) WHERE atomic_number='$1' ")
+fi
+# if not found
+if [[  -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+  exit 0
+fi
+
+echo $ELEMENT | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR
+do
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+done
