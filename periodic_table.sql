da3695dbed54d23e935083c2533df240076658e5
Thiago Ferreira <thiagorocha503@gmail.com>
2023-06-24 10:09:48 -0300

    refactor: Extract types table from properties table

--- periodic_table.sql
+++ periodic_table.sql
@@ -62,14 +62,55 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
-    type character varying(30),
     atomic_mass numeric(9,6) NOT NULL,
     melting_point_celsius numeric NOT NULL,
-    boiling_point_celsius numeric NOT NULL
+    boiling_point_celsius numeric NOT NULL,
+    type_id integer NOT NULL
 );
 
 
 ALTER TABLE public.properties OWNER TO freecodecamp;
+
+--
+-- Name: types; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.types (
+    type_id integer NOT NULL,
+    type character varying NOT NULL
+);
+
+
+ALTER TABLE public.types OWNER TO freecodecamp;
+
+--
+-- Name: types_type_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.types_type_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.types_type_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: types_type_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.types_type_id_seq OWNED BY public.types.type_id;
+
+
+--
+-- Name: types type_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.types ALTER COLUMN type_id SET DEFAULT nextval('public.types_type_id_seq'::regclass);
+
 
 --
 -- Data for Name: elements; Type: TABLE DATA; Schema: public; Owner: freecodecamp
@@ -90,15 +131,31 @@ INSERT INTO public.elements VALUES (1000, 'mT', 'moTanium');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008000, -259.1, -252.9);
-INSERT INTO public.properties VALUES (2, 'nonmetal', 4.002600, -272.2, -269);
-INSERT INTO public.properties VALUES (3, 'metal', 6.940000, 180.54, 1342);
-INSERT INTO public.properties VALUES (4, 'metal', 9.012200, 1287, 2470);
-INSERT INTO public.properties VALUES (5, 'metalloid', 10.810000, 2075, 4000);
-INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011000, 3550, 4027);
-INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007000, -210.1, -195.8);
-INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999000, -218, -183);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1.000000, 10, 100);
+INSERT INTO public.properties VALUES (1, 1.008000, -259.1, -252.9, 1);
+INSERT INTO public.properties VALUES (2, 4.002600, -272.2, -269, 1);
+INSERT INTO public.properties VALUES (6, 12.011000, 3550, 4027, 1);
+INSERT INTO public.properties VALUES (7, 14.007000, -210.1, -195.8, 1);
+INSERT INTO public.properties VALUES (8, 15.999000, -218, -183, 1);
+INSERT INTO public.properties VALUES (3, 6.940000, 180.54, 1342, 2);
+INSERT INTO public.properties VALUES (4, 9.012200, 1287, 2470, 2);
+INSERT INTO public.properties VALUES (5, 10.810000, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (1000, 1.000000, 10, 100, 3);
+
+
+--
+-- Data for Name: types; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.types VALUES (1, 'nonmetal');
+INSERT INTO public.types VALUES (2, 'metal');
+INSERT INTO public.types VALUES (3, 'metalloid');
+
+
+--
+-- Name: types_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.types_type_id_seq', 3, true);
 
 
 --
@@ -150,11 +207,27 @@ ALTER TABLE ONLY public.properties
 
 
 --
+-- Name: types types_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.types
+    ADD CONSTRAINT types_pkey PRIMARY KEY (type_id);
+
+
+--
 -- Name: properties fk_properties_elements; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.properties
     ADD CONSTRAINT fk_properties_elements FOREIGN KEY (atomic_number) REFERENCES public.elements(atomic_number);
+
+
+--
+-- Name: properties fk_properties_types; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.properties
+    ADD CONSTRAINT fk_properties_types FOREIGN KEY (type_id) REFERENCES public.types(type_id);
 
 
 --
